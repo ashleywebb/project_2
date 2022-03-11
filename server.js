233f498075619e6d3d7dd3ccd6f70f33a644ddb3
ashleywebb <ashleywebb112@gmail.com>
2022-03-11 00:22:34 -0700

    creating models folder

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const express = require('express')
 const morgan = require ('morgan')
 const methodOverride = require('method-override')
-const fruitController = require('./controllers/plans')
+const plansController = require('./controllers/plans')
 const path = require('path')
 
 //////////////////////
